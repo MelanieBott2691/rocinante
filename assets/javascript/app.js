23b91e35b43f2f4e26b184b37ce911380d222010
Robert Marks <robert.marks54@gmail.com>
2020-04-14 19:17:18 -0600

    js stuffs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,16 +2,12 @@
 // var data = require('./results.json');
 // hold 10 results from user search
 
-var from;
-var sizes;
+var from = 0;
+var sizes = 1;
 var tags;
-var q;
+var q = 'pancakes';
 
-from = 0;
-sizes = 1;
-q = 'pancakes';
-
-var randomFrom = function (max) {
+var randomIndex = function (max) {
     return Math.floor(Math.random() * Math.floor(max));
 };
 
@@ -24,14 +20,43 @@ var settings = {
         'x-rapidapi-key': '9084c1818dmshef24c102683f8f1p1d3041jsna84bad01aefb',
     },
 };
+
 $('.btn').on('click', function () {
     $.ajax(settings).done(function (response) {
         var resultCount = response.count;
-        from = randomFrom(resultCount + 1);
+        from = randomIndex(resultCount - 1);
         console.log(from);
         if (resultCount > 0) {
-            $.ajax(settings).done(function (response) {
-                console.log(response.results[0].id);
+            $.ajax({
+                url: 'https://tasty.p.rapidapi.com/recipes/list?from=' + from + '&size=' + sizes + '&tags&q=' + q,
+                method: 'GET',
+                timeout: 0,
+                headers: {
+                    'x-rapidapi-host': 'tasty.p.rapidapi.com',
+                    'x-rapidapi-key': '9084c1818dmshef24c102683f8f1p1d3041jsna84bad01aefb',
+                },
+            }).done(function (response) {
+                var res = response.results[0];
+                var id = res.id;
+                var name = res.name;
+                var recipes = res.recipes;
+                var recipeLength = recipes.length;
+                var yields = res.yields;
+                var sections = res.sections;
+                console.log(res);
+                console.log(id);
+                console.log(name);
+                if (recipes) {
+                    recipeIndex = randomIndex(recipeLength);
+                    recipe = recipes[recipeIndex];
+                    console.log(recipe.sections);
+                    console.log(recipe.yields);
+                } else {
+                    console.log(sections);
+                    console.log(yields);
+                }
+
+                var resultsWindow = $('<div>');
             });
         }
     });
