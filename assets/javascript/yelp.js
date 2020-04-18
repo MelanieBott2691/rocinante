7d48cb75e42163875175ad480a1137723a6abfb0
Robert Marks <robert.marks54@gmail.com>
2020-04-18 14:31:02 -0600

    final tweaks

--- assets/javascript/yelp.js
+++ assets/javascript/yelp.js
@@ -1,16 +1,16 @@
-var settings = {
-    url: 'http://api.yelp.com/v3/businesses/search?term=restaurant&location=84101',
-    method: 'GET',
-    timeout: 0,
-    headers: {
-        Authorization: 'Bearer Kkm2cU1Q9A33_YRm-DF34f2COXqO9oLZPUbGPbf6niprmm-uYtIqnAer-MDlOvlt7jhuO0BLbUVfghaU8WJtHJ27ToFYEDjs7LPO1YhhfqvKySzGMaAz1RKhikGaXnYx',
-        // Cookie: '__cfduid=da251eedb1e27d7f30e7df747308d1b751587194827',
-    },
-};
+// var settings = {
+//     url: 'http://api.yelp.com/v3/businesses/search?term=restaurant&location=84101',
+//     method: 'GET',
+//     timeout: 0,
+//     headers: {
+//         Authorization: 'Bearer Kkm2cU1Q9A33_YRm-DF34f2COXqO9oLZPUbGPbf6niprmm-uYtIqnAer-MDlOvlt7jhuO0BLbUVfghaU8WJtHJ27ToFYEDjs7LPO1YhhfqvKySzGMaAz1RKhikGaXnYx',
+//         Cookie: '__cfduid=da251eedb1e27d7f30e7df747308d1b751587194827',
+//     },
+// };
 
-console.log(count);
+// console.log(count);
 
-if (count % 5 === 0)
-    $.ajax(settings).done(function (response) {
-        console.log(response.businesses);
-    });
+// if (count % 5 === 0)
+//     $.ajax(settings).done(function (response) {
+//         console.log(response.businesses);
+//     });
